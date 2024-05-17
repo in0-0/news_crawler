5f0d2a91d772420cf163b3d5e889ddfda17a0f18
심인용 <inyong.shim@samsung.com>
2024-05-17 19:14:29 +0900

    [Refact] use value instead of constant

--- parser/naver.py
+++ parser/naver.py
@@ -18,6 +18,10 @@ class ParserNaver:
             "global": "262",
         }
 
+        self.title_content = "sa_text_title"
+        self.date_selector = "#ct > div.media_end_head.go_trans > div.media_end_head_info.nv_notrans > div.media_end_head_info_datestamp > div > span"
+        self.content_class = "article"
+
     def parse_articles_selenium(self, article_list):
         titles = article_list.find_elements(by=By.CLASS_NAME, value="sa_text")
         title_text = [
@@ -37,8 +41,9 @@ class ParserNaver:
     def parse_articles_bs(self, link):
         req = requests.get(link)
         soup = BeautifulSoup(req.content, "html.parser")
-        title_text = [t.text.strip("\n") for t in soup.find_all(class_="sa_text_title")]
-        links = [t["href"] for t in soup.find_all(class_="sa_text_title")]
+        titles = soup.find_all(class_=self.title_content)
+        title_text = [t.text.strip("\n") for t in titles]
+        links = [t["href"] for t in titles]
         articles = [self.get_article(l) for l in links]
 
         ret_dict = {
@@ -53,9 +58,7 @@ class ParserNaver:
         req = requests.get(link)
         soup = BeautifulSoup(req.content, "html.parser")
 
-        date = soup.select_one(
-            "#ct > div.media_end_head.go_trans > div.media_end_head_info.nv_notrans > div.media_end_head_info_datestamp > div > span"
-        )["data-date-time"]
-        content = soup.select_one("article").text
+        date = soup.select_one(self.date_selector)["data-date-time"]
+        content = soup.select_one(self.content_class).text
 
         return {"date": date, "content": content}
